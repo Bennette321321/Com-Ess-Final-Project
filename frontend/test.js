8cc10455b020071ea2f5031b226f140eb7ee5596
Napongthorn <napongthorn9009@gmail.com>
2024-04-18 10:00:43 +0000

    TMMM

--- frontend/test.js
+++ frontend/test.js
@@ -2,7 +2,3 @@ console.log("Hello world");
 console.log("Hello world 2")
 console.log("Hello world 3")
 console.log("Hello world 4")
-console.log("Hello world 5")
-console.log("Hello world 6")
-console.log("Helkiasdaj");
-console.log("kfjaslkdlak");
