f06ae050aead4e6e66d225cd265e2d3278f6e61d
Bennette321321 <6631321321@student.chula.ac.th>
2024-04-18 10:06:50 +0000

    Start Project

--- frontend/test.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello world");
-console.log("Hello world 2");
-console.log("Hello world 3");
-console.log("Hello world 4");
-console.log("Hello world 5");
-console.log("Hello world 6");
