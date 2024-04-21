93e9a380581bdf732fe6943c3c6c23a88fb5588c
Bennette321321 <6631321321@student.chula.ac.th>
2024-04-21 16:35:52 +0000

    MiniFix

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,15 +4,9 @@ import cors from "cors";
 import LeaderBoardRoute from "./routes/leaderboardRoute.js";
 
 const app = express();
-
-// body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// allow request from other origin (Frontend which is at different port)
 app.use(cors());
-
-// use routes
 app.use("/leaderboard", LeaderBoardRoute);
 
 export default app;
